0905ad28f3bccabf69f561985aadee6ccaa57bc4
Alperen <124510560+Nevers01@users.noreply.github.com>
2026-06-30 23:00:09 +0300

    v0.5.6 BugFix

--- widgets/sidebar.py
+++ widgets/sidebar.py
@@ -47,7 +47,7 @@ class Sidebar(QFrame):
 
         layout.addStretch()
 
-        footer = QLabel("Cortex ThinClient v0.5.4")
+        footer = QLabel("Cortex ThinClient v0.5.6")
         footer.setObjectName("SubTitle")
         layout.addWidget(footer)
 
